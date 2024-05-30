36dd98c474b5a33c8953ce7a2b8356ecd8e77fa4
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-05-30 16:44:20 +0200

    Added Meme component with initial state setup for meme generation

---
+++ src/components/Meme.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Meme() {
+ const [meme, setMeme] = React.useState({
+  topText: "",
+  bottomText: "",
+  randomImage: "http://i.imgflip.com/1bij.jpg",
+ });
+ const [allMemes, setAllMemes] = React.useState([]);
+}
